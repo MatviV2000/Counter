d761f916e67328ecb5db28351410fa0d8f1b56ee
VasiliyMatvienko <matvienko.vasiliy.a@gmail.com>
2025-04-26 23:34:27 +0300

    Выполнено базовое задние: добавлен label с отображением счетчика и базовым отображением счетчика 0 добавлена кнопка которая увеличивает значение на 1

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var counterAddButton: UIButton!
+    
+    var counter: Int = 0 {
+        didSet {
+            counterLabel.text = "Значение счётчика: \(counter)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonAddTouch(_ sender: Any) {
+        print("Нажатие")
+        counter += 1
+    }
 }
 

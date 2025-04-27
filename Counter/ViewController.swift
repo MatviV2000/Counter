2416a6796934effe96ad7cf4210d6fa059fff3a4
VasiliyMatvienko <matvienko.vasiliy.a@gmail.com>
2025-04-27 16:11:36 +0300

    Изменена иконка Блоки кода расставлены в порядке (Иниты, оверрайды, свойства и функции, Экшены (аутлеты пока поставил после оверрайдов. не совсем понял к чему они пренадлежат))

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,20 +9,18 @@ import Foundation
 import UIKit
 
 final class ViewController: UIViewController {
-
-    @IBOutlet weak private var counterLabel: UILabel!
-    @IBOutlet weak private var historyTextView: UITextView!
-    
-    
-    @IBOutlet weak private var incrementButton: UIButton!
-    @IBOutlet weak private var decrementButton: UIButton!
-    @IBOutlet weak private var resetButton: UIButton!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         incrementButton.setTitleColor(.systemRed, for: .normal)
         resetButton.setTitleColor(.systemGray, for: .normal)
     }
+    
+    @IBOutlet weak private var counterLabel: UILabel!
+    @IBOutlet weak private var historyTextView: UITextView!
+    
+    @IBOutlet weak private var incrementButton: UIButton!
+    @IBOutlet weak private var decrementButton: UIButton!
+    @IBOutlet weak private var resetButton: UIButton!
     
     private var counter: Int = 0 {
         didSet {
@@ -44,7 +42,7 @@ final class ViewController: UIViewController {
             )
         )
     }
-
+    
     @IBAction private func incrementButtonTouch(_ sender: Any) {
         print("Нажатие +1")
         
@@ -55,7 +53,7 @@ final class ViewController: UIViewController {
     
     @IBAction private func decrementButtonTouch(_ sender: Any) {
         print("Нажатие -1")
-
+        
         if counter > 0 {
             counter -= 1
             historyTextView.text += "\(formatter.string(from: Date.now)): значение изменено на -1\n"
@@ -67,7 +65,7 @@ final class ViewController: UIViewController {
     
     @IBAction private func resetButtonTouch(_ sender: Any) {
         print("Нажатие reset")
-
+        
         counter = 0
         historyTextView.text += "\(formatter.string(from: Date.now)) значение сброшено\n"
         scrollDown()
